c4cb3468f2ffb97b9be46f3593d40425c356f0cb
smorcor744 <148891340+smorcor744@users.noreply.github.com>
2024-12-12 20:08:23 +0100

    Inicial

---
+++ APITaller/src/main/kotlin/com/example/APITaller/ApiTallerApplication.kt
@@ -0,0 +1,13 @@
+package com.example.APITaller
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiTallerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiTallerApplication>(*args)
+
+}
+fun mai1() {}
---
+++ APITaller/src/test/kotlin/com/example/APITaller/ApiTallerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.APITaller
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiTallerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
