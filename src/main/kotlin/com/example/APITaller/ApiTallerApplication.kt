539e6477d16f04abb0cea1851f29030dd4752aa6
smorcor744 <mcsergio@iesrafaelalberti.es>
2024-12-12 20:03:07 +0100

    Inicial

---
+++ src/main/kotlin/com/example/APITaller/ApiTallerApplication.kt
@@ -0,0 +1,12 @@
+package com.example.APITaller
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiTallerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiTallerApplication>(*args)
+
+}
